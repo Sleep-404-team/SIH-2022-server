59a8ef12cdac6d1d66c2ec44b425ac589511647f
mdfarhaan <farhaanthestar@gmail.com>
2022-03-17 10:37:13 +0530

    Add Query

--- src/api/Internships/controller.js
+++ src/api/Internships/controller.js
@@ -1,11 +1,26 @@
 const db = require("../../shared/db");
 
 exports.getInternships = async (query) => {
-  const { location, preference } = query;
+  const { location, pref } = query;
 
   result = [];
-  const data = (await db()).collection("internships").find();
-  await data.forEach((item) => result.push(item));
+
+  if (location) {
+    const data = (await db())
+      .collection("internships")
+      .find({ location: location });
+    await data.forEach((item) => result.push(item));
+  } else if (pref) {
+    const prefQuery = pref.split(",");
+    const data = (await db())
+      .collection("internships")
+      .find({ tags: { $in: prefQuery } });
+    await data.forEach((item) => result.push(item));
+  } else {
+    const data = (await db()).collection("internships").find();
+    await data.forEach((item) => result.push(item));
+  }
+
   return result;
 };
 
